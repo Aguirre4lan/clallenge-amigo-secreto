622882215493e26982bc296db111183e030e75ab
Aguirre4lan <aguirreflavio84@hotmail.com>
2025-09-04 18:17:52 -0600

    Implementa función para agregar amigos con validación de entrada

--- app.js
+++ app.js
@@ -3,3 +3,24 @@
 
 // Array para almacenar los nombres de los amigos
 let amigos = [];
+
+// funcion para agregar amigos al array
+function agregarAmigo() {
+    // Captura el valor del campo de entrada
+    const input = document.getElementById("amigo");
+    // trim elimina los espacios en blanco al principio y al final del campo
+    const nombre = input.value.trim();
+    
+    //Validación de que no esta vacio el campo
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    //Actualiza el array de amigos
+    amigos.push(nombre);
+
+    //Limpia el campo de entrada
+    input.value = "";
+    
+}
